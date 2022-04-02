c4820b6a9096669589ce3fdd4907a7169d447e8e
m.magiera <m.magiera@avsystem.com>
2022-04-02 16:19:47 +0200

    swagger

--- src/main/java/com/forseti/forseti/controller/CategoryController.java
+++ src/main/java/com/forseti/forseti/controller/CategoryController.java
@@ -1,9 +1,22 @@
 package com.forseti.forseti.controller;
 
 import com.forseti.forseti.service.CategoryService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class CategoryController {
     private CategoryService categoryService;
+
+    @RequestMapping(value = "/categories", method = RequestMethod.GET)
+    public List<String> getCategories() {
+        List<String> productsList = new ArrayList<>();
+        productsList.add("Honey");
+        productsList.add("Almond");
+        return productsList;
+    }
 }
