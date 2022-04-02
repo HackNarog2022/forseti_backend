5d661ca494accea27187dfa697633fcba024cfcb
mimagiera <michalmagierra@gmail.com>
2022-04-02 18:22:07 +0200

    category service more methods

--- src/main/java/com/forseti/forseti/service/CategoryService.java
+++ src/main/java/com/forseti/forseti/service/CategoryService.java
@@ -1,11 +1,13 @@
 package com.forseti.forseti.service;
 
 import com.forseti.forseti.model.Category;
+import com.forseti.forseti.model.Inspiration;
 import com.forseti.forseti.repository.CategoryRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 @AllArgsConstructor
@@ -14,5 +16,9 @@ public class CategoryService {
 
     public List<Category> getAll() {
         return categoryRepository.findAll();
+    }
+
+    public void saveNewCategory(String name, Set<Inspiration> inspirations) {
+        this.categoryRepository.save(new Category(name, inspirations));
     }
 }
