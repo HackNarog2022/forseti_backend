857d941c67d32b8f8d3738fe00105b1e960f1ae0
mimagiera <michalmagierra@gmail.com>
2022-04-02 15:32:42 +0200

    Initial commit

---
+++ src/main/java/com/forseti/forseti/ForsetiApplication.java
@@ -0,0 +1,13 @@
+package com.forseti.forseti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForsetiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForsetiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forseti/forseti/ForsetiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forseti.forseti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForsetiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
