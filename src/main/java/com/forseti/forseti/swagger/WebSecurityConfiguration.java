a3da22778de8034c75cae8a21151d80b3fe2fa96
arkadiuss <arkadius.kraus@gmail.com>
2022-04-02 19:45:07 +0200

    Add azure b2c security

--- src/main/java/com/forseti/forseti/swagger/WebSecurityConfiguration.java
+++ src/main/java/com/forseti/forseti/swagger/WebSecurityConfiguration.java
@@ -1,21 +1,26 @@
 package com.forseti.forseti.swagger;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import com.azure.spring.cloud.autoconfigure.aad.AadJwtBearerTokenAuthenticationConverter;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers(
-                "/v2/api-docs",
-                "/configuration/ui",
-                "/swagger-resources/**",
-                "/configuration/**",
-                "/swagger-ui.html",
-                "/webjars/**"
-        );
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests((requests) -> requests.antMatchers("/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/**",
+                        "/swagger-ui.html",
+                        "/webjars/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(new AadJwtBearerTokenAuthenticationConverter());
     }
 }
