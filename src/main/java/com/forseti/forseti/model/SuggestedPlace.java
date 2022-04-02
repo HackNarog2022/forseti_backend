9490cf9fdddc677afb864ae1e3761afb21bdba81
mimagiera <michalmagierra@gmail.com>
2022-04-03 01:47:24 +0200

    add hardcoded suggested places

--- src/main/java/com/forseti/forseti/model/SuggestedPlace.java
+++ src/main/java/com/forseti/forseti/model/SuggestedPlace.java
@@ -1,11 +1,14 @@
 package com.forseti.forseti.model;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 @Data
+@Builder
 public class SuggestedPlace {
     @Id
     private String id;
