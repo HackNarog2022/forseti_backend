d17f13e37b5bde8ec20441b674505ac52a8b7e9c
Przemysław Jabłecki <prz.jab98@gmail.com>
2022-04-03 03:40:18 +0200

    Add email to user

--- src/main/java/com/forseti/forseti/model/User.java
+++ src/main/java/com/forseti/forseti/model/User.java
@@ -2,10 +2,10 @@ package com.forseti.forseti.model;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 public class User {
     @Id
     String id;
+    String email;
 }
